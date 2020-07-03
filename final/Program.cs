c1427c07a8e24334b3c1b23745cf8d0eb22abe4d
vince-huynh <vince.bhuynh@gmail.com>
2020-07-02 20:24:45 -0400

    third commit

--- final/Program.cs
+++ final/Program.cs
@@ -6,16 +6,38 @@ namespace final
     {
         static void Main(string[] args)
         {
-            SomeClass test = new SomeClass();
-            test.SomeClassMethod();
+            Person[] arr = new Person[2];
+            arr[0] = new Adult();
+            arr[0].FirstName = "adult firstname";
+            arr[0].LastName = "adult lastname";
+
+            arr[1] = new Child();
+            arr[1].FirstName = "child firstname";
+            arr[1].LastName = "child last name";
+
+            foreach (Person x in arr)
+            {
+                Console.WriteLine(x.Display());
+            }
         }
 
-        public class SomeClass 
+        public abstract class Person
         {
-            public void SomeClassMethod()
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Display()
             {
-                Console.WriteLine("Hello World!");
+                return FirstName + " " + LastName;
             }
+        }
+
+        public class Adult : Person
+        {
+        }
+
+        public class Child : Person
+        {
         }
     }
 }
